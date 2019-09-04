a543f4a862c8e9c8eec0f604b4e33d0a8f063e71
Erick Celio <erickcelio@hotmail.com>
2019-09-04 09:49:44 -0300

    Adicionando redux-persist para persistir dados

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import { Provider } from 'react-redux'
 import React from 'react'
 import Routes from './routes'
 import { ThemeProvider } from 'styled-components'
-import store from './store'
+import { store, persistor } from './store'
 import styles from './assets/styles'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const { styles: { Container } } = styles
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={{ mode: 'dark' }}>
-        <Container>
-          <Routes />
-        </Container>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor} >
+        <ThemeProvider theme={{ mode: 'dark' }}>
+          <Container>
+            <Routes />
+          </Container>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import auth from './ducks/auth'
+import storage from 'redux-persist/lib/storage'
 import { combineReducers, createStore } from 'redux'
+import { persistReducer, persistStore } from 'redux-persist'
 
 const reducers = combineReducers({
   auth
 })
 
-export default createStore(
-  reducers,
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+const store = createStore(
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+const persistor = persistStore(store)
+
+export {
+  store, persistor
+}
